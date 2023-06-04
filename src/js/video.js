5456efc47d2d4356350aba05e1370f414bbb2df8
NickTsalyn <nicktsalyn@gmail.com>
2023-06-04 12:51:16 -0400

    Update video.js

--- src/js/video.js
+++ src/js/video.js
@@ -1,17 +1,17 @@
 const video = document.querySelector('.video'),
-    playBtn = document.querySelector('.controls_play'),
-    playBtnImg = document.querySelector('.play_btn')
+  playBtn = document.querySelector('.controls_play'),
+  playBtnImg = document.querySelector('.play_btn');
 
 // Play and pause
 
 function toggleVideoStatus() {
-    if (video.paused) {
-        video.play()
-        playBtnImg.src = 'img/How_its_made/stop.svg'
-    } else {
-        video.pause()
-        playBtnImg.src = 'img/How_its_made/play.svg'
-    }
+  if (video.paused) {
+    video.play();
+    playBtnImg.src = 'img/How_its_made/play.svg';
+  } else {
+    video.pause();
+    playBtnImg.src = 'img/How_its_made/play.svg';
+  }
 }
-playBtn.addEventListener('click', toggleVideoStatus)
-video.addEventListener('click', toggleVideoStatus)
+playBtn.addEventListener('click', toggleVideoStatus);
+video.addEventListener('click', toggleVideoStatus);
